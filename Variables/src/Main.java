e32b201b0d70e4cbfc5820c8a64b5abe00746ee7
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-10 18:25:37 +0300

    int number=10 ve String message="Ogrenci Sayisi" eklendi ve ekrana yazdirildi.Bu sekilde degiskenlerle calisma yaptik.

---
+++ Variables/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int number=10;
+        String message="Ogrenci Sayisi";
+        System.out.println("ogrenci sayisi:10");
+        System.out.println("ogrenci sayisi:"+number);
+        System.out.println(message+number);
+        System.out.println(message+":"+number);
+    }
+}
