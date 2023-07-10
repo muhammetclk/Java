77a8402ee85192a88e3bc4ad1e725098004ab09e
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-10 23:09:39 +0300

    if ,else if ve else  ile calisma yapildi.

---
+++ conditionals/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    int number=20;
+
+	    if(number>20){
+	        System.out.println("Sayi 20 den buyuktur");
+        }
+	    else if(number==20){
+	        System.out.println("Sayi 20 ye esittir");
+        }
+	    else {
+	        System.out.println("Sayi 20 den kucuktur");
+        }
+    }
+}
