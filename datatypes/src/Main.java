8d4ad05e8bc73af7c458a3d9e10c2a233b3b8c0a
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-10 18:41:58 +0300

    veri tipleriyle calisma yapildi. int, String, char, double, boolean, byte , float, long

---
+++ datatypes/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //int, String, char, double, boolean, byte , float, long
+
+        double sayi=15.7;
+        sayi=128;
+        System.out.println(sayi);
+    }
+}
