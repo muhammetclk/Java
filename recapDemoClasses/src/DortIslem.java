c036fd90d8209e5294acb456adbcff6fb846f441
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-12 16:30:37 +0300

    DortIslem adinda bir class olustruduk ve temel matematiksel operasyonlari yaptirdik.

---
+++ recapDemoClasses/src/DortIslem.java
@@ -0,0 +1,15 @@
+public class DortIslem {
+
+    public void Topla(int sayi1,int sayi2){
+        System.out.println(sayi1+"+"+sayi2+"="+(sayi1+sayi2));
+    }
+    public void Carpma(int sayi1,int sayi2){
+        System.out.println(sayi1+"*"+sayi2+"="+(sayi1*sayi2));
+    }
+    public void Cikarma(int sayi1,int sayi2){
+        System.out.println(sayi1+"-"+sayi2+"="+(sayi1-sayi2));
+    }
+    public void Bolme(int sayi1,int sayi2){
+        System.out.println(sayi1+"/"+sayi2+"="+(sayi1/sayi2));
+    }
+}
---
+++ recapDemoClasses/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    DortIslem dortIslem=new DortIslem();
+	    dortIslem.Topla(4,5);
+    }
+}
