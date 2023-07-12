d0fc3be42fbcbe6801e79a0f895569b202ce9d76
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-12 15:36:16 +0300

    variable arguments ile calisma yapildi.

--- methods2/src/Main.java
+++ methods2/src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 	int result=topla(7,8);
 	System.out.println("result: "+result);
 	mesajVer();
+
+	int result2=topla2(1,2,3,4,5);
+	System.out.println("result2: "+result2);
+
     }
     public static int topla(int sayi1,int sayi2){
 
@@ -11,5 +15,15 @@ public class Main {
     }
     public static void mesajVer(){
         System.out.println("Parametresiz ve deger dondurmeyen metod.");
+    }
+
+    //variable arguments c# daki params ile ayni sey uc nokta ile yapiliyor.
+    public static int topla2(int ...sayilar){
+        int toplam=0;
+        for (int sayi:sayilar) {
+            toplam+=sayi;
+
+        }
+        return toplam;
     }
 }
