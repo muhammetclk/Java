ae6a4dd33c1ce2e62b7fe368abd2312dc1acf0ae
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-12 16:10:48 +0300

    ilk classimizi olusturduk.CustomerManager isimli classimizi olusturduk ve icinde musteri ekleme,guncelleme,silme yapan metodlari tanimladik.

---
+++ classes/src/CustomerManager.java
@@ -0,0 +1,12 @@
+public class CustomerManager {
+    public void Add(){
+        System.out.println("Customer Added");
+    }
+    public void Delete(){
+        System.out.println("Customer Deleted");
+    }
+    public void Update(){
+        System.out.println("Customer Updated");
+    }
+
+}
