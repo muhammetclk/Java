db60349c8941d4e792aee3dcefb90631d1c1c8fb
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-15 13:29:08 +0300

    interface lerin ilk kullanimi yapildi.Class ve abstract classtan farki goruldu.

---
+++ interfaces/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //bir class birden fazla interfac i implemente edebilir.
+        //interface i implemente ederken bir classi da extend edebilir.
+	    // interface lerde abstract gibi newlenemez.
+        //ICustomerDal iCustomerDal=new ICustomerDal();
+
+        ICustomerDal customerDal=new MySqlCustomerDal();// interface onu implemente eden class in referansini tutuyor.Polymorphism
+        customerDal.add();
+
+    }
+}
