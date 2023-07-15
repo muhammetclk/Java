c11b25a732110b29601c557fb1a3f7b16a5ba30c
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-15 13:53:22 +0300

    interface ile coklu implementasyon ornegi yaptik.Bir sirketin kendi calisani ve outsource calisani oldugunu dusunduk.Kendi calisanimiza Workable,payable,eatable operasyonlarni yaptirdik fakat outsource calisanina sadece workable operasyonunu uyguladik.Daha sonra yeni bir calisan olarak robotlarin geldigini dusunduk ve ona sadece workable operasyonunu uyguladik yine robotlara ozel bir operasyon olarak bakim operasyonunu da ekleyebilirdik.Bu sekilde interfacelerin calisma hayatinda kullanimina yaklasmaya calistik.

---
+++ interfaceDemo/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// bir sirketimiz var ve iki tip calisan var.
+        //kendi calisanimiz ve firma calisani
+        //kendi calisanimiza maas ve yemek parasini sirket veriyor.
+        //fakat firma calisanlarina maasi ve yemek parasini kendi firmalari veriyor.
+        //ortak ozellik olarak ikiside calisiyor.
+    }
+}
---
+++ interfaceDemo/src/Robot.java
@@ -0,0 +1,7 @@
+//yeni bir calisan olarak robotlarin geldigini dusunursek calisirlar ve bakimlari olabilir(buna ugun bir interface yazilabilir.) fakar maas ve yemek almazlar.
+public class Robot implements IWorkable {
+    @Override
+    public void work() {
+
+    }
+}
