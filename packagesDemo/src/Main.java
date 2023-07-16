64e4233cd6276d3490f5991658236ad5a2677396
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-16 15:05:46 +0300

    Kendi packages imizi yazdik ve icerisnde tanimladigimiz classlari kullandik.

--- packagesDemo/src/Main.java
+++ packagesDemo/src/Main.java
@@ -1,6 +1,10 @@
 //built-in  tanimli packages ler var demek.
 
 //util=utulity=araclar javadaki araclar icinden Scanner paketini kullanicaz.
+import matematik.DortIslem;
+import matematik.Logaritma;
+//import matematik.*  ile paketin iccerisindeki herseyi kullaniriz.
+
 import java.util.Scanner;
 public class Main {
 
@@ -12,5 +16,11 @@ public class Main {
         System.out.println("Isim giriniz: ");
         String  isim=scanner.nextLine();//String tipinde deger dondurur..
         System.out.println("isminiz: "+isim);
+
+        //kendi packages imizi yazdik ve Dortilem yazip tiklayinca otomatik olarak yukarda import etti.
+        DortIslem dortIslem=new DortIslem();
+        System.out.println(dortIslem.topla(2,3));
+        Logaritma logaritma=new Logaritma();
+
     }
 }
