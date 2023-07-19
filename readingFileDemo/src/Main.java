fc653c6b354e89b0a60bac60682fd00987713d22
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-19 15:20:22 +0300

    reading file yaparak checked exceptionlar ile calistik.

---
+++ readingFileDemo/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	    //dosya okumaya yonelik bir checked calismasi yapacagiz.
+        //once dosyayi bellege alip ordan okuma yapmamizi saglayan class BufferedReader.
+        //FileReader yapicaz ve src klaorune olusturdugumuz sayilar.txt yolunu veriyoruz.
+        //fakat java bu tarz islmeleri handle etmemizi istiyor.Bu islemi baskasi kullanacaksada  try blogu icinde calistirmamizi istiyor.
+        //Once FileReader uzerine gelip handle ettik.Uzerine gelip add exception to method signuture dedik.
+        //ikinci yolda FileReader uzerine gelip surround with try/catch diyerek bu blok icine almasini saglamak.
+
+        BufferedReader reader=null;//ilk once try da yapmistik fakat finally de ulasamadigimiz icin disarda tanimladik.
+        try {
+            //yoluna direk sayilar.txt yazinca bulamadi ve filenotfoundexception firlatti.
+             reader=new BufferedReader(new FileReader("D:\\javademos\\readingFileDemo\\src\\sayilar.txt"));
+        } catch (FileNotFoundException e) {//dosyayi bulamazsa hatasi
+            e.printStackTrace();
+        }
+        finally {//olusturdugumuz readeri kaptmamiz lazim ama try blogunun icinde oldugunda ulasamiyoruz.
+            //bu yuzden try icinde tanimlamizaz disarda tanimlicaz.
+            reader.close();// Dosyayi try da  okuyamazsa kapatilacak dosya olmayabilir o yuzden ilk denedigimizde yaptigimiz add exception to method signuture
+            //islemini yapmaliyiz. Oda yukari throw IOException firtlatir.
+            //veya finally icindeki islemi try catch icindede tanimlayabiliriz.
+        }
+
+    }
+}
