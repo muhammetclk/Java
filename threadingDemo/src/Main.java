95b5aba6d1574da7b35ef0ed6c2e40efbf6fe24f
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-22 15:32:05 +0300

    threade giris yapildi.Ve single thread ve multi thread arasindaki fark anlasildi

---
+++ threadingDemo/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// threading ikiye ayrili single threading ve multi threading
+        //single da birden fazla operasyon sirali sekilde icra edilir.Cunku bu operasyonlar birbirine baglidir
+        //multi threading de operasyonlar birbirine bagli degildir ve birden fazla thread yapisi olusturarak
+        //operasyonlari ayni anda calistirip sureden kazaniriz,.
+        //thread yapisini anlamak icin burda koronmetre ornegi yapicaz.
+        //bazi uygulamalarda birden fazla kronometre acilabilir bunun ornegi
+    }
+}
