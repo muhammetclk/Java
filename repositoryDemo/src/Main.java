1a75fa0d6ca38f0bc92231c19d4fa58b20047dde
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-22 15:09:28 +0300

    Generics metodlarla calistik

--- repositoryDemo/src/Main.java
+++ repositoryDemo/src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// repositry deseni veritabani islemleri icin kullanilir.Ekleme ,silme,guncelleme,select gibi
+        Validator validator=new Validator();
+        Customer customer=new Customer();
+        validator.validate(customer);
     }
 }
