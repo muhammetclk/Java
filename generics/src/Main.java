d5c5ea7ee7ee9ce63471d969cdfe0be2c7ee741c
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-22 14:29:46 +0300

    generics yapiya giris yapildi.

---
+++ generics/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Mylist<String> sehirler=new Mylist<String>();
+//        sehirler.add("Ankara");
+//        sehirler.add("Istanbul");
+//        sehirler.add("Izmir");
+
+        Mylist<Customer> musteriler=new Mylist<>();
+        musteriler.add(new Customer());
+    }
+}
---
+++ generics/src/Mylist.java
@@ -0,0 +1,9 @@
+public class Mylist<T> {//ArrayList yerine gecen bir yapiyi generics seklinde olusturucaz.
+    public void add(T value ){
+
+    }
+    public void remove(T value ){
+
+    }
+
+}
