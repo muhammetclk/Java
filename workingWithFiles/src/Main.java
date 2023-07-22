532f5f53b3d81325f7aed6d8ca51cc6b7ae5ca18
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-22 13:31:54 +0300

    Dosyanin bilgilerini getirdik.Dosyannin adi,yolu,yazilabilrimi,okunabilirmi,ve boyutu nu ogrendik.

--- workingWithFiles/src/Main.java
+++ workingWithFiles/src/Main.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args)  {
+        getFileInfo();
+
+    }
+    public static void createFile(){
         File file=new File("D:\\javademos\\files\\students.txt");//dosyalari verdigimiz bu yola eklicez.
         //Ayrica direk yola olusturacagimiz dosyanin adini uzantisiyla bilikte ekleyince olusuyor.
         try {
@@ -16,6 +20,16 @@ public class Main {
 
         } catch (IOException e) {
             e.printStackTrace();//hatayi metin olarak ekrana yazioyr.
+        }
+    }
+    public static void getFileInfo(){
+        File file=new File("D:\\javademos\\files\\students.txt");
+        if(file.exists()){//dosya varmi kontrol ediyor.
+            System.out.println("Dosya adi: "+file.getName());
+            System.out.println("Dosya yolu: "+file.getAbsolutePath());
+            System.out.println("Dosya yazilabilir mi ?: "+file.canWrite());
+            System.out.println("Dosya okunabilir mi ?: "+file.canRead());
+            System.out.println("Dosyanin boyutu : "+file.length());
         }
     }
 }
