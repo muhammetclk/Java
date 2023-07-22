c5adfbc987faf9c3b842d834234c5240450d59b8
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-22 17:06:47 +0300

    Threadlerle ilgili gercek hayat uygulamasi yapildi ve threadlerin calisma mantigi anlasildi.

--- threadingDemo/src/Main.java
+++ threadingDemo/src/Main.java
@@ -7,5 +7,12 @@ public class Main {
         //operasyonlari ayni anda calistirip sureden kazaniriz,.
         //thread yapisini anlamak icin burda koronmetre ornegi yapicaz.
         //bazi uygulamalarda birden fazla kronometre acilabilir bunun ornegi
+
+        KronometreThread thread1=new KronometreThread("Kronometre1");
+        KronometreThread thread2=new KronometreThread("Kronometre2");
+        KronometreThread thread3=new KronometreThread("Kronometre3");
+        thread1.start();
+        thread2.start();
+        thread3.start();
     }
 }
