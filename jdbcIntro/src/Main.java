4cfd23f3ab3718eea809cbf3009ddcc36fc5b437
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-23 15:13:30 +0300

    Insert islemi yapildi.

--- jdbcIntro/src/Main.java
+++ jdbcIntro/src/Main.java
@@ -8,6 +8,34 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
         dbHelper helper = new dbHelper();
+        PreparedStatement statement = null;//insert islemleri PreparedStatemnt ile yapiliyor.Kullanci veriyi kendi girdigi icin.
+        ResultSet resultSet;
+
+
+        Connection connection = null;
+        try {
+            connection = helper.getConnection();
+            String sql="insert into city(name,CountryCode,District,Population) values(?,?,?,?)";
+            //soru isareti olan yerlere assagida belirttigimiz degerler gelir.
+            statement=connection.prepareStatement(sql);
+            statement.setString(1,"Duzce 2");
+            statement.setString(2,"Tur");
+            statement.setString(3,"Turkey");
+            statement.setInt(4,700000);
+            statement.executeUpdate();//calistirir ve etkilenen kayit sayisini dondurur.
+            System.out.println("Kayit eklendi.");
+
+        } catch (SQLException exception) {
+            helper.showErrorMessage(exception);
+        } finally {
+            connection.close();
+        }
+
+
+
+    }
+    public static void selectDemo() throws SQLException{
+        dbHelper helper = new dbHelper();
         Statement statement = null;//sql cumlecigini tutuyoruz..
         ResultSet resultSet;//sql sonucu Resultset olarak javada karsilanioyr.Yani sonuc burda tutuluyor.
 
@@ -32,7 +60,6 @@ public class Main {
         } finally {
             connection.close();
         }
-
 
     }
 }
