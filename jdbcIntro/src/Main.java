d2cb9d8b2ed6a2424b58b93d68638be3e977ca74
Muhammet Çelik <muhammetclk56@gmail.com>
2023-07-23 13:30:50 +0300

    Sql sorgumuzu yazdik ve bunu calstirarak veritabanindan istedigimiz veriyi cekip ekrana bastik.

--- jdbcIntro/src/Main.java
+++ jdbcIntro/src/Main.java
@@ -1,22 +1,28 @@
+import javax.lang.model.element.Name;
 import java.net.ConnectException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
 
 
     public static void main(String[] args) throws SQLException {
-        dbHelper helper=new dbHelper();
-        Connection connection=null;
-        try{
+        dbHelper helper = new dbHelper();
+        Statement statement = null;//sql cumlecigini tutuyoruz..
+        ResultSet resultSet;//sql sonucu Resultset olarak javada karsilanioyr.Yani sonuc burda tutuluyor.
 
-            connection=helper.getConnection();
-            System.out.println("Baglanti olustu");
-        }catch (SQLException exception){
+
+        Connection connection = null;
+        try {
+
+            connection = helper.getConnection();
+            statement=connection.createStatement();//gonderilecek sorgu bu baglantiya gidecek.
+            resultSet=statement.executeQuery("Select Code,Name,Continent,Region from country");//sorguyu calistiracak.
+            while (resultSet.next()){//tek tek yaz anlamina geliyor.
+                System.out.println(resultSet.getString("Name"));//name alanini bas
+            }
+        } catch (SQLException exception) {
             helper.showErrorMessage(exception);
-        }
-        finally {
+        } finally {
             connection.close();
         }
 
